aa73cc9316e5bd56ea254d23227a927f540d1ef0
Chisom Worlu <worluchisom4real@gmail.com>
2026-01-15 01:32:32 +0100

    portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ function onOpenCaseStudy(){
       {/* BACKGROUND */}
       <div className="fixed inset-0 -z-10">
         <div
-          className="absolute top-0 left-0 w-[600px] h-[600px] bg-cyan-500/15 blur-[140px] rounded-full transition-transform duration-700 ease-out"
+          className="absolute top-0 left-0 w-150 h-150 bg-cyan-500/15 blur-[140px] rounded-full transition-transform duration-700 ease-out"
           style={{
             transform: `translate(${mousePosition.x * 0.015}px, ${
               mousePosition.y * 0.015
@@ -52,14 +52,14 @@ function onOpenCaseStudy(){
           }}
         />
         <div
-          className="absolute bottom-0 right-0 w-[500px] h-[500px] bg-purple-600/12 blur-[120px] rounded-full transition-transform duration-700 ease-out"
+          className="absolute bottom-0 right-0 w-125 h-125 bg-purple-600/12 blur-[120px] rounded-full transition-transform duration-700 ease-out"
           style={{
             transform: `translate(${-mousePosition.x * 0.01}px, ${
               -mousePosition.y * 0.01
             }px)`
           }}
         />
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[400px] h-[400px] bg-blue-500/8 blur-[100px] rounded-full" />
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-100 h-100 bg-blue-500/8 blur-[100px] rounded-full" />
       </div>
 
       <div className="max-w-6xl mx-auto px-6 py-20">
